bb3fdb961dd45b30b95c83c787626ff1c89c6657
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-26 16:04:15 +0530

    learned variables and data types

---
+++ day02_data_types.py
@@ -0,0 +1,34 @@
+# Python supports various data types that allow you to store different kinds of information.
+# The most common data types in Python include: int, float, str, bool, list, tuple, dict, set
+# 1. Integer (int): Represents whole numbers without a decimal point.
+age = 34 # integer
+# 2. Float (float): Represents numbers with a decimal point.
+cgpa = 4.55 # float
+# 3. String (str): Represents a sequence of characters enclosed in quotes.
+name = "Vaheeda" # string
+# 4. Boolean (bool): Represents a value that can be either True or False.
+is_student = True # boolean
+# 5. List (list): Represents an ordered collection of items that can be of different types.
+fruits = ["apple", "banana", "cherry"] # list
+# 6. Tuple (tuple): Represents an ordered collection of items that cannot be changed (immutable).
+coordinates = (10.0, 20.0) # tuple
+# 7. Dictionary (dict): Represents a collection of key-value pairs.
+person = {"name": "Alice", "age": 30, "city": "New York"} # dictionary
+# 8. Set (set): Represents an unordered collection of unique items.
+unique_numbers = {1, 2, 3, 4, 5} # set
+# You can use the type() function to check the data type of a variable.
+
+age = 3
+print(age)
+print(type(age))
+
+cgpa = 8.3
+print(cgpa)
+print(type(cgpa))
+
+name = "Vaheeda"
+print(name)
+print(type(name))
+
+is_completed = True # can also be False
+print(is_completed)
---
+++ day02_variable.py
@@ -0,0 +1,17 @@
+# In Python, variables are used to store data that can be used and manipulated throughout a program. Avariables is created the moment you assign a value to it using the assignment opertor (=)
+
+age = 34 # integer
+name = "Vaheeda" # string
+cgpa = 4.55 # float
+
+# Rules for defining a variable in python:
+# 1. A variable name must start with a letter (a-z, A-Z) or an underscore (_).
+# 2. A variable name cannot start with a number (e.g., 1age is invalid).
+# 3. A variable name can only contain alphanumeric characters (letters and numbers) and underscores.
+# 4. Variable names are case-sensitive (age and Age are different variables).
+# Avoid using python reserved keywords as variable names (e.g., if, else, while, for, etc.)
+# Example of invalid variable names:
+# 1age = 30 # invalid because it starts with a number
+age = 30 # valid variable can start with number 
+# 2name = "John" # invalid because it starts with a number
+# @age = 30 # invalid because it starts with a special character
