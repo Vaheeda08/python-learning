802d039c9a5575dadbf239ec27af5751b38e4431
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-26 18:12:07 +0530

    user input

--- day02_input.py
+++ day02_input.py
@@ -5,7 +5,7 @@ a = int(a)  # convert the string to an integer
 print(a + 3)
 
 a = int(input("Enter a number: "))
-b = int(input("Enter another number:"))
+b = int(input("Enter another number: "))
 
 
 print(a + b)  # this will now add the numbers as integers
