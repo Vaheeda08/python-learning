66ae0f40a7c7ccd205d5e40056ce03bc8eab1771
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-26 17:03:47 +0530

    git commit -m "Fix typos in day02_variable.py"

--- day02_variable.py
+++ day02_variable.py
@@ -1,4 +1,4 @@
-# In Python, variables are used to store data that can be used and manipulated throughout a program. A variables is created the moment you assign a value to it using the assignment opertor (=)
+# In Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assignment operator (=)
 
 age = 34 # integer
 name = "Vaheeda" # string
