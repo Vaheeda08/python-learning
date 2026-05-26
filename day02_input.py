3cafe9cb5cd24009a58633a9cb6002b2c9147c5a
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-26 18:11:10 +0530

    user input

--- day02_input.py
+++ day02_input.py
@@ -1,10 +1,11 @@
-a = input ("Enter a number: ")
+# Day 02 - Taking User Input in Python
+
+a = input("Enter a number: ")  
 a = int(a)  # convert the string to an integer
 print(a + 3)
 
-a = int(input ("Enter a number:"))
-a = int(a)  
-b = int(input ("Enter another number:"))
-b = int(b)
+a = int(input("Enter a number: "))
+b = int(input("Enter another number:"))
+
 
 print(a + b)  # this will now add the numbers as integers
