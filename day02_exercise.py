731f0244526e9498081db1cb87021b08f1cf2df6
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-26 20:07:44 +0530

    Exercise set 1

---
+++ day02_exercise.py
@@ -0,0 +1,42 @@
+# Question 1 
+print("Hello, Python Learner")
+print("I am \"good\"")
+
+# Question 2 
+print("Hello, World! Welcome to Python")
+
+# Question 3
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!''')
+
+# Question 4
+name = "Alice"
+age = 30
+height = 1.65
+is_student = True
+print("The name of the student is " + name + ". She is " + str(age) + " years old. " + "She is " + str(height) + " cms tall. " + "Student: " + str(is_student))
+
+# Question 5
+num = "45"
+print(int(num)+10)
+
+# Question 6
+food = input("What is your favorite food?\n")
+print("Wow I also like " + food)
+
+# Question 7
+a = int(input("Enter a number:\n"))
+b = int(input("Enter another number:\n"))
+print("a + b = ", a + b)
+print("a - b = ", a - b) 
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+
+# Question 8
+print('Harry said, "I am learning Python!"\nThis is on a new line. \nThis is a tab ->\t<-')
+
+# Question 9
+a = int(input("Enter a number:\n"))
+print("The square of the number is: ", a**2)
+print("The cube of the number is: ", a**3)
