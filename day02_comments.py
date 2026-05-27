e966c932d92989eb1265016639d289ae4115ebc6
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-27 17:43:07 +0530

    comments

--- day02_comments.py
+++ day02_comments.py
@@ -1,9 +1,16 @@
+# Day 02 - Comments in Python
+
+# Comments are used to explain code and are ignored by Python.
+# Single line comments use #
+# Multi-line comments use triple quotes ''' '''
+
 print("Hey how are you?")
 
 #Arjun please finish this code
 
 
 #Rohan please review what Arjun has done
+
 '''
 #In Python, the input() function is used to take input from the user.
 # It always returns the input as a string, so we need to convert it
