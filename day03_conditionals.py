d86aabeb055f9d3538e6e14d3a2e056513c9513f
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-27 17:14:51 +0530

    conditionals

---
+++ day03_conditionals.py
@@ -0,0 +1,38 @@
+# if_statement
+
+age=12
+
+if(age>18):
+    print("You are an adult.")
+    print("You can vote.")
+
+
+print("End of program.")
+ 
+ # if_else_statement
+
+age = int(input("Enter your age: "))
+
+if(age>18):
+    print("You are an adult.")
+    print("You can vote.")
+else:
+    print("You are a minor.")
+    print("You cannot vote.")
+
+print("End of program.")
+
+# if_elif_else_statement
+
+age = int(input("Enter your age: "))
+
+if(age>18):
+    print("You are an adult.")
+    print("You can vote.")
+elif(age==13):
+    print("You are a teenager.")
+else:
+    print("You are a child.")
+    print("You cannot vote.")
+
+print("End of program.")
