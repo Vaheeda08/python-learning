d280564ef2b263cddfb74ac79d327a346fc9cf9e
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-27 17:37:33 +0530

    print

--- day02_print.py
+++ day02_print.py
@@ -1,6 +1,6 @@
-#print('Hello \' world')
+# Day 02 - Print Statement in Python
 
-print('Hello World', "Harry",5, sep="'")
+print('Hello World', "Harry",5, sep="'") #sep is used to separate the values with a specific character
 
-print('Hello World', end='..')
+print('Hello World', end='..') #end is used to specify what to print at the end of the statement. By default, it is a newline character (\n)
 print('Harry')
