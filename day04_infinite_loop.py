f22a4878ff5e7488515118c60560a522e4ad4eaa
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-28 03:12:04 +0530

    infinite_loops

--- day04_infinite_loop.py
+++ day04_infinite_loop.py
@@ -1,6 +1,8 @@
-# Day 04 = infinite loop
+# Day 04 - Infinite Loop
 
 i = 1
 while True:
     print(i)
     i = i + 1
+
+# Warning: This code will run indefinitely until you stop it manually. Press Ctrl + C to stop the execution.
