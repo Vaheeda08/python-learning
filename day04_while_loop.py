7590cd6d8fba631f7308b2dbd470be7b2c1d111e
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-28 03:00:41 +0530

    while_loop

---
+++ day04_while_loop.py
@@ -0,0 +1,17 @@
+# Day 04 = While Loop
+
+# While loop is used to execute a block of code repeatedly as long as a certain condition is true.
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+i = 1
+while i < 6:
+    print(i)
+    i = i + 1
+
+
+    
