4c9f10814f33608a5b34a47dd0cae87e591742d9
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-28 03:16:44 +0530

    variables

--- day02_variable.py
+++ day02_variable.py
@@ -1,3 +1,4 @@
+# Day 02 - Variables
 # In Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assignment operator (=)
 
 age = 34 # integer
