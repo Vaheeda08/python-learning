c26d5096f2b9bfa642ac52645ed9e0ef78970732
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-29 15:00:19 +0530

    break

---
+++ day05_break.py
@@ -0,0 +1,8 @@
+# Day 05 - Break Statement
+# The break statement is used to exit a loop prematurely when a certain condition is met.
+
+for i in range(0, 21):
+    print(i)
+    if i == 11:
+        break #Cancel the execution of this loop now
+    
