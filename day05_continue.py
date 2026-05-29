5581d0c67f4eb95f42590bf8055feb3a0dd91046
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-29 15:10:14 +0530

    continue

---
+++ day05_continue.py
@@ -0,0 +1,9 @@
+ # Day 05 - continue
+# The continue statement is used to skip the current iteration of a loop and move on to the next iteration.
+
+for i in range(1, 20):
+    if i == 10:
+        continue #Skip the rest of the code in this loop and move on to the next iteration
+    print(i) #print 1-9, skips 10, then prints 11-19
+
+    
