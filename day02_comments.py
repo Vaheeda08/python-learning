d1090e1a0488806c4e1fa14a1bdf0c1ae705f6e6
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-30 15:26:50 +0530

    comments

--- day02_comments.py
+++ day02_comments.py
@@ -12,8 +12,8 @@ print("Hey how are you?")
 #Rohan please review what Arjun has done
 
 '''
-#In Python, the input() function is used to take input from the user.
-# It always returns the input as a string, so we need to convert it
-# to the required data type using typecasting. For example, int() converts
-# the input to an integer so we can perform mathematical operations on it.
+In Python, the input() function is used to take input from the user.
+It always returns the input as a string, so we need to convert it
+to the required data type using typecasting. For example, int() converts
+the input to an integer so we can perform mathematical operations on it.
 '''
