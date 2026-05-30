8820925f0f557d23f195ebe1176e299e6bce17ae
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-30 15:30:36 +0530

    input

--- day02_input.py
+++ day02_input.py
@@ -2,10 +2,10 @@
 
 a = input("Enter a number: ")  
 a = int(a)  # convert the string to an integer
-print(a + 4)
+print(a + 3)
 
 a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-
+b =2
+int(input("Enter another number: "))
 
 print(a + b)  # this will now add the numbers as integers
