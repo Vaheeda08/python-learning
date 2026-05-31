24d3cf3eab7f0e6a45f0108c431c99b914c463e0
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-05-31 23:10:52 +0530

    Exercise-2

--- day06_exercise.py
+++ day06_exercise.py
@@ -1,5 +1,77 @@
 # Day 06 - Exercise
 
 # Question 1
+from re import match
+
+
 for i in range(1, 11):
     print(f"56 x {i:2} = {56 * i:3}")
+
+# Question 2
+num = int(input("Enter a number: "))
+print(num)
+
+if(num<0):
+    print("Negative")
+
+elif(num>0):
+    print("Positive")
+
+else:
+    print("Zero")
+
+# Question 3
+age = int(input("Enter your age: "))
+
+if (age>=18):
+    print("You are eligible to vote.")
+
+else:
+    print("You are not eligible to vote.")
+
+# Question 4
+num = int(input("Enter a number\n"))
+
+if(num%2==0):
+    print("Even")
+
+else:
+    print("Odd")
+
+# Question 5
+num = int(input("Enter a number\n"))
+
+match num:
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wednesday")
+    case 5:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday")
+
+# Question 6
+num = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+operation = input("Choose an operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        print(f"{num} + {num2} = {num + num2}")
+    case "-":
+        print(f"{num} - {num2} = {num - num2}")
+    case "*":
+        print(f"{num} * {num2} = {num * num2}")
+    case "/":
+        if(num2!=0):
+            print(f"{num} / {num2} = {num / num2}")
+        else:
+            print("Cannot divide by zero.")
