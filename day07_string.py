d8ed0300de49f9009eb828fea3af118c171ae425
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-01 17:27:23 +0530

    string

--- day07_string.py
+++ day07_string.py
@@ -1,4 +1,4 @@
-# Day 7: String Methods
+# Day 07 - String Methods
 # String methods are built-in functions that allow you to manipulate and work with strings in Python. Here are some commonly used string methods:
 # 1. `lower()`: Converts all characters in a string to lowercase.
 # 2. `upper()`: Converts all characters in a string to uppercase.
@@ -14,11 +14,13 @@ print(text.upper())  # Output: "  HELLO, WORLD!  "
 print(text.strip())  # Output: "Hello, World!"
 print(text.replace("World", "Python"))  # Output: "  Hello, Python!  "
 print(text.split(", "))  # Output: ['  Hello', 'World!  ']
+
 words = ["Hello", "World"]
 print(" ".join(words))  # Output: "Hello World"
 # These string methods can be very useful for various text processing tasks, such as formatting, cleaning, and manipulating strings in your Python programs.
 
-name = 'Vaheeda'
-name = "Vaheeda"
-name = '''Vaheeda is a good girl'''
+name = 'Vaheeda'  #single quotes
+name = "Vaheeda"  #double quotes
+name = '''Vaheeda is a good girl'''  #triple quotes
+
 print(name)  # Output: Vaheeda is a good girl
