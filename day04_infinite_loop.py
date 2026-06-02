848cbeadf8194a7664406b63e83ee057c0af3202
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-02 21:45:36 +0530

    infinite loop

--- day04_infinite_loop.py
+++ day04_infinite_loop.py
@@ -3,7 +3,7 @@
 i = 2
 while True:
     print(i)
-    i = i + 2
+    i = i + 3
 
-    i = i + 2
+    i = i + 3
 # Warning: This code will run indefinitely until you stop it manually. Press Ctrl + C to stop the execution.
