b1910215b17df8a1c2f60234cbd77d34b24629d9
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-02 21:20:57 +0530

    string method

---
+++ day08_string_methods.py
@@ -0,0 +1,21 @@
+# Day 08 - String Methods
+# Python provides several built-in methods for manipulating strings. Here are some commonly used string methods:
+# 1. `upper()`: Converts all characters in a string to uppercase.
+# 2. `lower()`: Converts all characters in a string to lowercase.
+# 3. `capitalize()`: Capitalizes the first character of a string and converts the rest to lowercase.
+# 4. `title()`: Capitalizes the first character of each word in a string.
+# 5. `strip()`: Removes leading and trailing whitespace from a string.
+# 6. `replace(old, new)`: Replaces occurrences of a specified substring with another substring.
+# 7. `split(separator)`: Splits a string into a list of substrings based on a specified separator.
+# 8. `join(iterable)`: Joins elements of an iterable (like a list) into a single string, using a specified separator.
+# Example usage of string methods:
+# Example string
+name = "Vaheeda" # Strings are immutable
+a = len(name)
+print(a) # Output: 7
+print(name.upper()) # Output: VAHEEDA
+print(name.lower()) # Output: vaheeda
+print(name.capitalize()) # Output: Vaheeda
+print(name.title()) # Output: Vaheeda
+print(name.strip()) # Output: Vaheeda
+print(name.replace("Vaheeda", "Fatima")) # Output: Fatima
