bde5fbb6ab9c576405fdc5f0d27705ee6f7d9160
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-02 21:43:28 +0530

    fstrings

---
+++ day08_fstrings.py
@@ -0,0 +1,18 @@
+# Day 08 - String Formatting with f-strings
+# F-strings are a way to format strings in Python. They are a more concise and readable way to format strings than the older .format() method.
+# To create an f-string, you simply prefix the string with the letter 'f' or 'F' and then use curly braces {} to include expressions that will be evaluated and included in the string.
+
+template = "Dear Vaheeda, You are awesome. Take this 1000$ bag of money as a gift from me. Best, Your friend"
+a = "Koina"
+a1 = "1000$"
+b = "Katherine"
+b1 = "500$"
+c = "Caroline"
+c1 = "200$"
+
+# Using f-strings to format the string with variables
+
+s1 = template.format(a, a1)
+print(s1)
+
+print(f"Dear {a}, You are awesome. Take this {a1} bag of money as a gift from me. Best, Your friend")
