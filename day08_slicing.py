8187e9391a7c2e95123c0397595c1fa1a10ae273
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-02 21:09:59 +0530

    slicing

--- day08_slicing.py
+++ day08_slicing.py
@@ -7,11 +7,13 @@ name = "Vaheeda"
 
 print(name[0:2])  # goes from 0 to 2-1 (2 is not included)
 
-print(name[2:-1])  # Same as name [2:4]
+print(name[2:-1])  # Output: heed
 
-print(name[0:10:1])  # skip n-1 characters 
-print(name[0:10:2])  # skip 1 character
-print(name[0:10:3])  # skip 2 characters
+print(name[0:10:1])  # step 1 = every character, Output: Vaheeda
+print(name[0:10:2])  # step 2 = skip 1 character, Output: Vhda
+print(name[0:10:3])  # step 3 = skip 2 characters, Output: Vae
 
-print(name[:4]) # Replace the first empty number with 0
-print(name[1:5]) # Replace the second empty number with the length #name[1:5]
+print(name[:4]) # Replace the first empty number with 0, Output: Vahe
+print(name[1:5]) # Replace the second empty number with the length #name[1:5], Output: aheed
+
+print(name[::-1]) # Reverse the string, Output: adeehav
