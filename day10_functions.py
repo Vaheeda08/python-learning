783e69c0f1144aa387d1a0b6abf39d79d6a59acb
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-04 19:48:31 +0530

    functions

---
+++ day10_functions.py
@@ -0,0 +1,4 @@
+# Day 10 = Functions
+# Functions are reusable blocks of code that perform a specific task. They help to break down complex problems into smaller, manageable pieces, and they can be called multiple times throughout a program.
+# Here's a simple example of a function that takes two numbers as input and returns their sum:
+
