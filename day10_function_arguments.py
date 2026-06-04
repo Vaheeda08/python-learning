465115b67e7825393c50fa162beac0342be2bcfe
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-04 20:07:57 +0530

    arguments

---
+++ day10_function_arguments.py
@@ -0,0 +1,12 @@
+# Day 10 = Functions Arguments
+# Functions can have arguments, which are values that you can pass to the function when you call it.
+# You can define a function with parameters, which are placeholders for the arguments that will be passed to the function.
+# Here's an example of a function that takes two parameters and returns their sum:
+
+def add(x, y, plus=0):
+    return x + y + plus
+result = add(3, 5)
+print(result)
+
+c = add(10, 20, 8)
+print(c)
