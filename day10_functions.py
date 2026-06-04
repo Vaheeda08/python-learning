676fffb1b0530bd55d4d8487440b29581788b643
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-04 19:49:45 +0530

    functions

--- day10_functions.py
+++ day10_functions.py
@@ -2,3 +2,9 @@
 # Functions are reusable blocks of code that perform a specific task. They help to break down complex problems into smaller, manageable pieces, and they can be called multiple times throughout a program.
 # Here's a simple example of a function that takes two numbers as input and returns their sum:
 
+a = 4
+b = 2
+c = 1
+
+average = (a + b + c) / 3
+print(average)
