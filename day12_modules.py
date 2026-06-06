6013cd0bcc4ca782e6cf2b22e9c01e176d1ebe10
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-06 13:30:56 +0530

    modules

---
+++ day12_modules.py
@@ -0,0 +1,14 @@
+# Day 12 - Modules
+# A module is a file that contains Python code. It can define functions, classes, and variables. You can import a module to use its functionality in your code.
+
+# Two types of modules:
+# 1. Built-in modules: These are modules that come with Python. You can use them without installing anything. Examples include math, random, datetime, etc.
+# 2. Third-party modules: These are modules that are not included with Python but can be installed using package managers like pip. Examples include numpy, pandas, requests, etc.
+# To import a module, you can use the import statement. For example, to import the math module, you can do:
+
+import math
+
+print(math.sqrt(16))  # Output: 4.0
+
+from math import sqrt
+print(sqrt(25))  # Output: 5.0
