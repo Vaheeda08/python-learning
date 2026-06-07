bcf65930367b56e62aa336580ff0d938b650efb3
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-07 11:33:56 +0530

    function_scope

--- day13_function_scope.py
+++ day13_function_scope.py
@@ -1,4 +1,5 @@
 # Day 13 - Function Scope
+# Scope refers to the region of a program where a variable is defined and can be accessed. In Python, there are two main types of scope: global and local.
 
 def sum(a, b):
     # a and b are local variables
@@ -13,3 +14,5 @@ def greet():
 z = 8 # z is a global variable
 print(sum(4, 6))
 print(z) # This will print 8 because z is defined in the global scope
+
+greet()  # add this at the bottom
