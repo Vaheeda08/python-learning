ad6b36f9ed1f47d838e369f7d27485e44dccfa33
Vaheeda Fatima <fatimavaheeda95@gmail.com>
2026-06-07 11:25:24 +0530

    global

---
+++ day13_global.py
@@ -0,0 +1,14 @@
+# Day 13 - Global Variables
+# Global variables are variables that are defined outside of a function and can be accessed and modified by any function in the program.
+# Global variables are useful when you want to share data between different functions or when you want to maintain a state that is accessible throughout the program.
+
+def sum(a, b):
+    print("Hey I am summing")
+    c = a + b
+    global z #Please modify global z
+    z = 0 # This will refer to global z and not create a local variable z
+    return c
+
+z = 3
+print(sum(3, 12))
+print(z) # This will print 0 because we modified the global variable z in the sum function
